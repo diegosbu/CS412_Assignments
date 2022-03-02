01e8b31a38d01ab928e91ce3359f514f97523b16
Diego Sanchez <diegos@bu.edu>
2022-03-02 18:23:53 -0500

    Problem 1 part D completed

--- PS2/routes/ps3.js
+++ PS2/routes/ps3.js
@@ -11,4 +11,9 @@ router.post('/', function (req, res, next) {
   res.render('index', {origStr: bInput, strLen: bInput.length});
 });
 
+router.get('/names/:name', function (req, res, next) {
+  name1 = req.params.name
+  res.render('index', {name: name1})
+});
+
 module.exports = router;

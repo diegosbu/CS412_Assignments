a3548bcb53e9908b07bee37f038a12ef430c5ad4
Diego Sanchez <diegos@bu.edu>
2022-04-09 15:52:02 -0400

    first commit

--- PS1/PS1.P1.js
+++
@@ -1,6 +0,0 @@
-//revAlph - sorts string in reverse alphabetical order
-const revAlph = (word) => {
-    return word.split('').sort().reverse().join('');
-}
-
-console.log(revAlph('supercalifragilisticexpialidocious'));

2e26ed9c8e5c6690bdeb8157677095060f53d853
Diego Sanchez <diegos@bu.edu>
2022-04-09 21:51:41 -0400

    async call function implemented

--- PS4/routes/ps4.js
+++ PS4/routes/ps4.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 const request = require('request');
+const fetch = require('node-fetch');
 
-require('dotenv').config()
+require('dotenv').config();
 
-const API_KEY = process.env.APIKEY
+const API_KEY = process.env.APIKEY;
 
 // API used: themoviedb
 
@@ -13,7 +14,7 @@ router.get('/', (req, res, next) => {
   res.send('the different routes are: /ps4/promise, /ps4/async, and /ps4/callback');
 });
 
-router.post('/promise/', (req, res, next) => {
+router.post('/promise', (req, res, next) => {
   const doReq = () => {
     return new Promise((resolve, reject) => {
       const options = {
@@ -42,5 +43,40 @@ router.post('/promise/', (req, res, next) => {
       console.log(err)
     })
 });
+
+
+router.post('/async', (req, res, next) => {
+  const doReq = async() => {
+    const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`, {method: 'GET'});
+    const data = await response.json();
+
+    return data;
+  }
+
+  doReq()
+    .then(data => {
+      console.log(data)
+      res.send(data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
+});
+
+
+// router.post('/callback', (req, res, next) => {
+//   const doReq = async() => {
+//     const response = await fetch(`https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}`, {method: 'GET'});
+//     const data = await response.json();
+
+//     return data;
+//   }
+
+//   constsendReq = operator => {
+//     response = await doReq()
+//     res.send(data)
+//   }
+
+// });
 
 module.exports = router;

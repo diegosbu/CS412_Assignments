824f6f7be93ff3d568add759ce503ee1bee6011e
Diego Sanchez <diegos@bu.edu>
2022-04-09 20:18:00 -0400

    promise wrapper for request call implemented

--- PS4/routes/ps4.js
+++ PS4/routes/ps4.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const request = require('request');
 
 require('dotenv').config()
 
@@ -8,12 +9,38 @@ const API_KEY = process.env.APIKEY
 // API used: themoviedb
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.send('the different routes are: /ps4/promise, /ps4/async, and /ps4/callback');
 });
 
-router.post('/promise', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/promise/', (req, res, next) => {
+  const doReq = () => {
+    return new Promise((resolve, reject) => {
+      const options = {
+        method: 'GET',
+        url: 'https://api.themoviedb.org/3/genre/movie/list',
+        qs: {api_key: API_KEY}
+      }
+  
+      request(options, (err, res, body) => {
+        if (err) {
+          throw new Error(err)
+        } else {
+          resolve(body)
+        }
+      })
+    })
+  }
+
+  doReq()
+    .then(body => {
+      const response = JSON.parse(body)
+      console.log(response)
+      res.send(response)
+    })
+    .catch(err => {
+      console.log(err)
+    })
 });
 
 module.exports = router;
